904fbc112e7bc6ed9623da8b6d9064ab8141cd00
jacerfil <jacer.filho@escola.pr.gov.br>
2026-08-03 09:15:47 -0300

    3

---
+++ script.js
@@ -0,0 +1,8 @@
+function mostrarMensagem() {
+
+    const mensagem = document.getElementById("mensagem");
+
+    mensagem.innerHTML =
+    "Todos merecem respeito. Se você presenciar um caso de cyberbullying, denuncie e ajude a vítima.";
+
+}
